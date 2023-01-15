f5f8e667ab74e4cb87807cc69156b928461770db
Yunseonghwan <sunghwan0208@naver.com>
2023-01-15 17:43:18 +0900

    fix header, auth

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,13 +1,35 @@
+import { gql, useQuery } from "@apollo/client";
 import { isLoggedInVar } from "../apollo";
+import { meQuery } from "../__generated__/meQuery";
+
+const ME_QUERY = gql`
+  query meQuery {
+    me {
+      id
+      email
+      role
+      verified
+    }
+  }
+`;
 
 const LoggedInRouter: React.FC = () => {
+  const { data, loading, error } = useQuery<meQuery>(ME_QUERY);
+
+  if (!data || loading || error) {
+    return (
+      <div className=" h-screen flex justify-center items-center">
+        <span className=" font-medium text-xl tracking-wide">loading...</span>
+      </div>
+    );
+  }
+
   const onClick = () => {
     isLoggedInVar(false);
   };
   return (
     <div>
-      <h1>logged In</h1>
-      <button onClick={onClick}>Click to LoggedOut</button>
+      <h1>{data.me.email}</h1>
     </div>
   );
 };
